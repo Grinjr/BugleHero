df18ca1eb4d5fc51e29b1413a6e5bb2e169d351e
Grinjr <Grinjr@yahoo.com>
2025-08-17 03:53:25 -0700

    Fixed issue where multiple notes playing can cause an infinite looping note

--- BugleHero/MidiInputHandler.cs
+++ BugleHero/MidiInputHandler.cs
@@ -6,13 +6,19 @@ using BepInEx.Configuration;
 using System.Collections.Concurrent;
 using UnityEngine;
 using System;
+using System.Collections.Generic;
 
 public class MidiInputHandler
 {
 	private MidiIn midiIn;
 	private ConfigEntry<string> midiDeviceName;
 
+
 	private readonly ConcurrentQueue<Action> mainThreadQueue = new ConcurrentQueue<Action>();
+
+	// Track expected note states to detect stuck notes
+	private readonly HashSet<int> expectedActiveNotes = new HashSet<int>();
+	private float lastMidiActivity = 0f;
 
 	private void EnqueueOnMainThread(Action a)
 	{
@@ -21,6 +27,7 @@ public class MidiInputHandler
 
 	public void ProcessMainThreadQueue()
 	{
+		// Process MIDI actions
 		while (mainThreadQueue.TryDequeue(out var action))
 		{
 			try { action(); }
@@ -28,6 +35,22 @@ public class MidiInputHandler
 			{
 				Plugin.Instance.mls.LogWarning("Exception processing MIDI action: " + ex);
 			}
+		}
+
+		// Safety check for stuck notes due to missed MIDI messages
+		CheckForStuckNotes();
+	}
+
+	private void CheckForStuckNotes()
+	{
+		// If we haven't received MIDI input for a while but think notes are active, something might be stuck
+		if (expectedActiveNotes.Count > 0 && Time.time - lastMidiActivity > 10f)
+		{
+			Plugin.Instance.mls.LogWarning($"No MIDI activity for 10 seconds but {expectedActiveNotes.Count} notes expected active. Clearing expected notes.");
+			expectedActiveNotes.Clear();
+
+			// Also force stop everything as a safety measure
+			BugleMIDIPatch.EmergencyStopAll();
 		}
 	}
 
@@ -50,6 +73,8 @@ public class MidiInputHandler
 			new ConfigDescription("Select MIDI input device", null,
 				new AcceptableValueList<string>(deviceNamesList.ToArray()))
 		);
+
+
 
 		midiDeviceName.SettingChanged += (s, e) =>
 		{
@@ -91,12 +116,15 @@ public class MidiInputHandler
 	{
 		try
 		{
+			lastMidiActivity = Time.time;
+
 			if (e.MidiEvent is NoteOnEvent noteOn)
 			{
 				int midiNote = noteOn.NoteNumber;
 
 				if (noteOn.Velocity > 0)
 				{
+					expectedActiveNotes.Add(midiNote);
 					EnqueueOnMainThread(() =>
 					{
 						var bugleInstance = FindLocalBugleSFX();
@@ -108,6 +136,8 @@ public class MidiInputHandler
 				}
 				else
 				{
+					// Some MIDI devices send Note On with velocity 0 instead of Note Off
+					expectedActiveNotes.Remove(midiNote);
 					EnqueueOnMainThread(() =>
 					{
 						var bugleInstance = FindLocalBugleSFX();
@@ -120,6 +150,7 @@ public class MidiInputHandler
 			}
 			else if (e.MidiEvent is NoteEvent noteEvent && noteEvent.CommandCode == MidiCommandCode.NoteOff)
 			{
+				expectedActiveNotes.Remove(noteEvent.NoteNumber);
 				EnqueueOnMainThread(() =>
 				{
 					var bugleInstance = FindLocalBugleSFX();
@@ -128,6 +159,26 @@ public class MidiInputHandler
 						BugleMIDIPatch.OnMidiNoteOff(noteEvent.NoteNumber, bugleInstance);
 					}
 				});
+			}
+			else if (e.MidiEvent is ControlChangeEvent ccEvent)
+			{
+				// Handle common MIDI CC messages that might affect note state
+				// CC 120 = All Sound Off, CC 123 = All Notes Off
+				if (ccEvent.Controller == (MidiController)120 ||
+					ccEvent.Controller == (MidiController)123)
+				{
+					Plugin.Instance.mls.LogInfo($"Received All Notes/Sound Off MIDI message (CC {(int)ccEvent.Controller})");
+					expectedActiveNotes.Clear();
+					EnqueueOnMainThread(() =>
+					{
+						BugleMIDIPatch.EmergencyStopAll();
+					});
+				}
+				else if (ccEvent.Controller == MidiController.Sustain)
+				{
+					// Handle sustain pedal - you might want to modify this behavior
+					Plugin.Instance.mls.LogInfo($"Sustain pedal: {ccEvent.ControllerValue}");
+				}
 			}
 		}
 		catch (Exception ex)
@@ -139,10 +190,17 @@ public class MidiInputHandler
 	private void MidiIn_ErrorReceived(object sender, MidiInMessageEventArgs e)
 	{
 		Plugin.Instance.mls.LogWarning("MIDI error: " + e.RawMessage);
+
+		// On MIDI errors, clear our expected state as it might be unreliable
+		expectedActiveNotes.Clear();
 	}
 
 	public void StopListening()
 	{
+		// Clean up when stopping
+		expectedActiveNotes.Clear();
+		BugleMIDIPatch.EmergencyStopAll();
+
 		if (midiIn != null)
 		{
 			midiIn.Stop();
@@ -155,5 +213,13 @@ public class MidiInputHandler
 	{
 		return UnityEngine.Object.FindObjectsOfType<BugleSFX>()
 			.FirstOrDefault(b => b.photonView.IsMine);
+	}
+
+	// Manual method to force stop all notes - you can call this from console commands or debug UI
+	public void EmergencyStopAllNotes()
+	{
+		Plugin.Instance.mls.LogInfo("Manual emergency stop triggered");
+		expectedActiveNotes.Clear();
+		BugleMIDIPatch.EmergencyStopAll();
 	}
 }
--- BugleHero/Patches/BugleMIDIPatch.cs
+++ BugleHero/Patches/BugleMIDIPatch.cs
@@ -3,19 +3,28 @@ using UnityEngine;
 using Photon.Pun;
 using System;
 using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BugleHero.Patches
 {
 	[HarmonyPatch(typeof(BugleSFX))]
 	internal class BugleMIDIPatch
 	{
-		private static int? currentNote = null;
+		// Track all active notes instead of just one
+		private static HashSet<int> activeNotes = new HashSet<int>();
+		private static int? currentPlayingNote = null; // The note currently being played (bugle can only play one at a time)
+
 		private static int forcedClip = 0;
 		private static float forcedPitch = 0f;
 		private static bool forceHold = false;
 
 		private static float loopStartTime = 0f;
 		private static float loopClipLength = 0f;
+
+		// Cleanup timer - if no activity for X seconds, force stop everything
+		private static float lastActivityTime = 0f;
+		private static readonly float inactivityTimeout = 5f;
 
 		const float loopRestartDelay = 0.05f;
 
@@ -65,7 +74,18 @@ namespace BugleHero.Patches
 		{
 			if (instance == null) return;
 
-			currentNote = midiNote;
+			lastActivityTime = Time.time;
+
+			// Add to active notes
+			activeNotes.Add(midiNote);
+
+			// If we're already playing a note, stop it first
+			if (currentPlayingNote.HasValue)
+			{
+				ForceStopCurrentNote(instance);
+			}
+
+			currentPlayingNote = midiNote;
 
 			int clipIndex = MidiNoteToClip(midiNote);
 			float basePitch = MidiNoteToPitch(midiNote);
@@ -77,10 +97,71 @@ namespace BugleHero.Patches
 		public static void OnMidiNoteOff(int midiNote, BugleSFX instance)
 		{
 			if (instance == null) return;
-			if (currentNote != midiNote) return;
 
-			StopToot(instance);
-			currentNote = null;
+			lastActivityTime = Time.time;
+
+			// Remove from active notes
+			activeNotes.Remove(midiNote);
+
+			// Only stop if this was the currently playing note
+			if (currentPlayingNote == midiNote)
+			{
+				StopToot(instance);
+				currentPlayingNote = null;
+
+				// If there are other active notes, play the most recent one
+				if (activeNotes.Count > 0)
+				{
+					int nextNote = activeNotes.Last(); // Play the most recently pressed note
+					OnMidiNoteOn(nextNote, instance);
+				}
+			}
+		}
+
+		// Emergency cleanup function
+		public static void ForceStopAllNotes(BugleSFX instance)
+		{
+			if (instance == null) return;
+
+			Plugin.Instance.mls.LogInfo("Force stopping all stuck notes");
+
+			activeNotes.Clear();
+			currentPlayingNote = null;
+			forceHold = false;
+
+			instance.hold = false;
+			tField?.SetValue(instance, false);
+			instance.photonView.RPC("RPC_EndToot", RpcTarget.All);
+
+			if (instance.buglePlayer.isPlaying)
+			{
+				instance.buglePlayer.Stop();
+			}
+
+			// Stop particles
+			if (instance.particle1 && instance.particle1.isPlaying)
+			{
+				instance.particle1.Stop();
+			}
+			if (instance.particle2 && instance.particle2.isPlaying)
+			{
+				instance.particle2.Stop();
+			}
+		}
+
+		private static void ForceStopCurrentNote(BugleSFX instance)
+		{
+			if (!currentPlayingNote.HasValue) return;
+
+			forceHold = false;
+			instance.hold = false;
+			tField?.SetValue(instance, false);
+			instance.photonView.RPC("RPC_EndToot", RpcTarget.All);
+
+			if (instance.buglePlayer.isPlaying)
+			{
+				instance.buglePlayer.Stop();
+			}
 		}
 
 		static void PlayToot(BugleSFX instance, int clipIndex, float pitch)
@@ -115,7 +196,6 @@ namespace BugleHero.Patches
 			}
 
 			instance.StartCoroutine(RemoteStartAfterDelay(instance, 0.05f, clipIndex, pitch));
-			//instance.photonView.RPC("RPC_StartToot", RpcTarget.Others, clipIndex, pitch);
 		}
 
 		public static void StopToot(BugleSFX instance)
@@ -131,6 +211,22 @@ namespace BugleHero.Patches
 		static bool Prefix_UpdateTooting(BugleSFX __instance)
 		{
 			if (!__instance.photonView.IsMine) return true;
+
+			// Check for stuck notes due to inactivity
+			if (forceHold && Time.time - lastActivityTime > inactivityTimeout)
+			{
+				Plugin.Instance.mls.LogWarning("Detected potentially stuck note due to inactivity, forcing stop");
+				ForceStopAllNotes(__instance);
+				return false;
+			}
+
+			// Additional safety check - if we think we're holding but bugle player isn't playing
+			if (forceHold && !__instance.buglePlayer.isPlaying && Time.time - loopStartTime > loopClipLength + 1f)
+			{
+				Plugin.Instance.mls.LogWarning("Detected stuck note (player not playing), forcing stop");
+				ForceStopAllNotes(__instance);
+				return false;
+			}
 
 			if (forceHold)
 			{
@@ -152,14 +248,31 @@ namespace BugleHero.Patches
 		static System.Collections.IEnumerator RestartAfterDelay(BugleSFX instance, float delay)
 		{
 			yield return new WaitForSeconds(delay);
-			instance.photonView.RPC("RPC_StartToot", RpcTarget.Others, forcedClip, forcedPitch);
-			PlayToot(instance, forcedClip, forcedPitch);
+
+			// Additional safety check before restarting
+			if (forceHold && currentPlayingNote.HasValue)
+			{
+				instance.photonView.RPC("RPC_StartToot", RpcTarget.Others, forcedClip, forcedPitch);
+				PlayToot(instance, forcedClip, forcedPitch);
+			}
 		}
 
 		static System.Collections.IEnumerator RemoteStartAfterDelay(BugleSFX instance, float delay, int clip, float pitch)
 		{
 			yield return new WaitForSeconds(delay);
 			instance.photonView.RPC("RPC_StartToot", RpcTarget.Others, clip, pitch);
+		}
+
+		// Public method you can call from elsewhere to manually fix stuck notes
+		public static void EmergencyStopAll()
+		{
+			var bugleInstance = UnityEngine.Object.FindObjectsOfType<BugleSFX>()
+				.FirstOrDefault(b => b.photonView.IsMine);
+
+			if (bugleInstance != null)
+			{
+				ForceStopAllNotes(bugleInstance);
+			}
 		}
 	}
 }
